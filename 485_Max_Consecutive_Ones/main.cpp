30fa76304776be397c9b993a05618477b84bc71e
hankyang0324 <hankyang0324@gmail.com>
2017-12-22 22:37:04 -0500

    Day 1 Array Easy 4

--- 485_Max_Consecutive_Ones/main.cpp
+++ 485_Max_Consecutive_Ones/main.cpp
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/max-consecutive-ones/description/
+
 //Given a binary array, find the maximum number of consecutive 1s in this array.
 #include <iostream>
 #include <vector>
